9e12268329743b8cc79af0f9d371a43abc3737b1
Xavier Collins <111817636+xcollins2001@users.noreply.github.com>
2022-12-03 15:25:10 -0500

    new

--- itis3135/scripts/polygons.js
+++ itis3135/scripts/polygons.js
@@ -1,9 +1,10 @@
 function generatePolygon(){
-    var polygonNumber = prompt("Hi, I am the xenoistic cat. Sounds funny right. Well I would like you to choose a number between 1 and 10 to match the polygon.")
+    let polygonNumber = prompt("Hi, I am the xenoistic cat. Sounds funny right. Well I would like you to choose a number between 1 and 10 to match the polygon.")
     validateEntry(polygonNumber);
 }
 
-function validateEntry(number){
+function validateEntry(number)
+{
     if(isNaN(number))
     {
         alert("Invalid selection!! Again, please choose a number between 1 and 10 match the polygon.");
@@ -11,7 +12,7 @@ function validateEntry(number){
     }
      else
      {
-        polygonSides = math.trunc(number);
+        polygonSides = Math.trunc(number);
         if(polygonSides < 1 || polygonSides > 10)
         {
             alert("Invalid selection!! Again, please choose a number between 1 and 10 match the polygon.");
@@ -24,8 +25,8 @@ function validateEntry(number){
      }
 
      function shapeOfPolygon(sides){
-            var typeOfPolygons = new Array ("Monogon", "Bigon", "Triangle", "quadrilateral", "pentagon", "hexagon", "heptagon", "octagon", "nonagon", "decagon");
-            var output = "The polygon that you have chosen has " + sides + " sides and the name of the polygon you've chosen is" + typeOfPolygons[sides - 1] + " ! ";
-            document.getElementById("arealPolygon").innerHTML = output;
+            var typeOfPolygons = new Array ("Monogon", "Bigon", "Triangle", "Quadrilateral", "Pentagon", "Hexagon", "Heptagon", "Octagon", "Nonagon", "Decagon");
+            var output = "The polygon that you have chosen has " + sides + " sides and the name of the polygon you've chosen is a " + typeOfPolygons[sides - 1] + " ! ";
+            document.getElementById("aRealPolygon").innerHTML = output;
      }
 }
