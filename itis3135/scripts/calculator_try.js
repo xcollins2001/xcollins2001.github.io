5e81357496dbb4454d4da6d268a2c4970060aaca
Xavier Collins <111817636+xcollins2001@users.noreply.github.com>
2022-12-04 16:20:05 -0500

    new

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,47 @@
+function getValue(){
+    let input = Event.target.innerText;
+    console.log(input)
+    printValue(input);
+}
+
+function printValue(val){
+    let out = document.querySelector('#result');
+    let current = out.innerHTML;
+
+    if(out.innerHTML == "0")
+    {
+        if(val !='C' && val !="DEL"){
+            out.innerHTML= "";
+            out.innerHTML += val;
+        }
+    }
+    else{
+        if(val =="DEL"){
+            out.innerHTML = current.slice(0,-1);
+            if(out.innerHTML.length <=1){
+                out.innerHTML = "0";
+            }
+        }
+        if(val != "C" && val!="DEL" && val!="="){
+            out.innerHTML += val;
+        }
+        if(val =="C"){
+            out.innerHTML = "0";
+        }
+
+        if(val =="="){
+            let res = out.innerHTML;
+            out.innerHTML = eval(res);
+        }
+    }
+}
+
+
+
+let buttons = document.getElementsByTagName('button');
+for(var i = 0; i<buttons.length; i++){
+    buttons[i].setAttribute('onClick', 'getValue()');
+}
+
+
+    
