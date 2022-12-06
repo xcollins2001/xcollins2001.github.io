f6d329ef000e1cf3608f0e4cfdff3e3545b9018c
Xavier Collins <111817636+xcollins2001@users.noreply.github.com>
2022-12-06 12:40:02 -0500

    new

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,47 +1,70 @@
-function getValue(){
-    let input = Event.target.innerText;
-    console.log(input)
-    printValue(input);
-}
-
-function printValue(val){
-    let out = document.querySelector('#output');
-    let current = out.innerHTML;
-
-    if(out.innerHTML == "0")
-    {
-        if(val !='C' && val !="DEL"){
-            out.innerHTML= "";
-            out.innerHTML += val;
+window.onload = function () {
+    const calculate = (n1, operator, n2) => {
+        let result = ''
+        if (operator === 'add') {
+          result = parseFloat(n1) + parseFloat(n2)
+        } else if (operator === 'subtract') {
+          result = parseFloat(n1) - parseFloat(n2)
+        } else if (operator === 'multiply') {
+          result = parseFloat(n1) * parseFloat(n2)
+        } else if (operator === 'divide') {
+          result = parseFloat(n1) / parseFloat(n2)
         }
-    }
-    else{
-        if(val =="DEL"){
-            out.innerHTML = current.slice(0,-1);
-            if(out.innerHTML.length <=1){
-                out.innerHTML = "0";
+      
+        return result
+      }
+      
+      const calculator = document.querySelector('.calculator')
+      const display = calculator.querySelector('.calculator__display')
+      const keys = calculator.querySelector('.calculator__keys')
+      
+      keys.addEventListener('click', function (e) {
+        if (e.target.matches('button')) {
+          const key = e.target
+          const action = key.dataset.action
+          const keyContent = key.textContent
+          const displayedNum = display.textContent
+          const previousKeyType = calculator.dataset.previousKeyType
+      
+          Array.from(key.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'))
+      
+          if (!action) {
+            if (displayedNum === '0' || previousKeyType === 'operator') {
+              display.textContent = keyContent
+            } else {
+              display.textContent = displayedNum + keyContent
             }
+          }
+      
+          if (action === 'decimal') {
+            display.textContent = displayedNum + '.'
+          }
+      
+          if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+          ) {
+            key.classList.add('is-depressed')
+            calculator.dataset.previousKeyType = 'operator'
+            calculator.dataset.firstValue = displayedNum
+            calculator.dataset.operator = action
+          }
+      
+          if (action === 'clear') {
+            console.log('clear key!')
+            alert("Calculator Cleared!");
+          }
+      
+          if (action === 'calculate') {
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNum
+      
+            display.textContent = calculate(firstValue, operator, secondValue)
+          }
         }
-        if(val != "C" && val!="DEL" && val!="="){
-            out.innerHTML += val;
-        }
-        if(val =="C"){
-            out.innerHTML = "0";
-        }
-
-        if(val =="="){
-            let res = out.innerHTML;
-            out.innerHTML = eval(res);
-        }
+      })
     }
-}
-
-
-
-let buttons = document.getElementsByTagName('button');
-for(var i = 0; i<buttons.length; i++){
-    buttons[i].setAttribute('onClick', 'getValue()');
-}
-
-
-    
